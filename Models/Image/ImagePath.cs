488bb81b362a98019d04fe0cab92ed61ed93a5b9
mandooplz <kmw4561@gmail.com>
2025-08-29 17:04:50 +0900

    updaet Models.Image.Processor

--- Models/Image/ImagePath.cs
+++ Models/Image/ImagePath.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Models.Image;
 
 
-// Object
+// Value
+public readonly record struct ImageFilePath
+{
+    // core
+    public required string Name { get; init; }
+    public required Ext Extension { get; init; }
 
 
+    // operator
+    override public string ToString()
+    {
+        return Name + "." + Extension.RawValue;
+    }
+
+
+    // value
+    public readonly record struct Ext
+    {
+        // core
+        public required string RawValue { get; init; }
+
+        public static readonly Ext PNG = new() { RawValue = "png" };
+        public static readonly 
+            
+            Ext JPG = new() { RawValue = "jpg" };
+    }
+}
--- Models/Image/Processor.cs
+++ Models/Image/Processor.cs
@@ -1,19 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SixLabors.ImageSharp;
+using System.Collections.Concurrent;
 
 namespace Models.Image;
 
 // Object
-public class Processor: IDisposable
+public class Processor
 {
+    // core
+    public static Processor Create()
+    {
+        var processorRef = new Processor();
+
+        ProcessorManager.Register(processorRef);
+
+        return processorRef;
+    }
+    private Processor() { }
+    internal void Delete()
+    {
+        ProcessorManager.Unregister(Id);
+    }
+
+
+    // state
+    public ID Id { get; } = ID.Random();
+
 
     // action
-    public void Dispose()
+    public void Remove()
     {
-        Console.WriteLine("객체가 정리됩니다.");
+        Console.WriteLine("객체가 제거됩니다.");
+    }
+
+
+    // value
+    public readonly record struct ID
+    {
+        // core
+        public required Guid RawValue { get; init; }
+
+        public static ID Random() => new ID { RawValue = Guid.NewGuid() };
     }
 }
 
 
 // ObjectManager
+internal static class ProcessorManager
+{
+    internal static ConcurrentDictionary<Processor.ID, Processor> container = new();
+    internal static void Register(Processor obj)
+    {
+        container.TryAdd(obj.Id, obj);
+    }
+    internal static void Unregister(Processor.ID id)
+    {
+        container.TryRemove(id, out _);
+    }
+}
